10aaddd50df1c1144cf28c054a8caf80ff594b93
Richard Memory <richardfmemory@gmail.com>
2019-04-14 16:26:51 -0600

    Add menu

--- app/src/main/java/com/example/android/github/MainActivity.java
+++ app/src/main/java/com/example/android/github/MainActivity.java
@@ -1,10 +1,14 @@
 
 package com.example.android.github;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mSearchBoxEditText;
@@ -23,5 +27,22 @@ public class MainActivity extends AppCompatActivity {
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
 
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+        if (menuItemThatWasSelected == R.id.action_search) {
+            Context context = MainActivity.this;
+            String message = "Search clicked";
+            Toast.makeText(context, message, Toast.LENGTH_LONG);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
